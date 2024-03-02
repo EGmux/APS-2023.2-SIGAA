64649370cfd1de123f5157055c693cf50e4d0b7e
Enzo Bissoli <enzogurgel@forge.localhost.localdomain>
2024-03-02 19:58:08 -0300

    update(main.go): testcode

--- src_go/main.go
+++ src_go/main.go
@@ -9,7 +9,11 @@ var db = make(map[string]string)
 
 func main() {
 	println("test 🤛")
+<<<<<<< HEAD
 	queries.TableExists("credentials")
+=======
+	queries.CheckExistence("credentials")
+>>>>>>> 95fd195 (update(main.go): testcode)
 	controllers.Set_Login_Controller()
 	// Listen and Server in 0.0.0.0:8080
 }
--- src_go/packages/utils/abstractfactory/abstractfactory.go
+++ src_go/packages/utils/abstractfactory/abstractfactory.go
@@ -11,3 +11,51 @@ type IAbstractFactory interface {
 	CreatePROAESRepo() bool
 	CreateProfessorRepo() bool
 }
+<<<<<<< HEAD
+=======
+
+type SQLRepoSitory struct {
+	conn *pgxpool.Pool
+}
+
+// TODO: implement
+// If table exits return else create it
+func (repo *sqlrepo) CreateCourseRepo() bool {
+	return true
+}
+
+// TODO: implement
+func (repo *sqlrepo) CreateCredentialRepo() bool {
+	return true
+}
+
+// TODO: implement
+func (repo *sqlrepo) CreatePROAESRepo() bool {
+	return true
+}
+
+// TODO: implement
+func (repo *sqlrepo) CreateTranscriptRepo() bool {
+	return true
+}
+
+// TODO: implement
+func (repo *sqlrepo) CreateEnrollmentRepo() bool {
+	return true
+}
+
+// TODO: implement
+func (repo *sqlrepo) CreateProfessorRepo() bool {
+	return true
+}
+
+// TODO: implement
+func (repo *sqlrepo) CreateClassRepo() bool {
+	return true
+}
+
+// TODO: implement
+func (repo *sqlrepo) CreateSubjectRepo() bool {
+	return true
+}
+>>>>>>> 95fd195 (update(main.go): testcode)
