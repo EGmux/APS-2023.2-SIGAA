3327b58bd680809cb4c3c884cfbec73300f6f56f
Enzo Bissoli <enzogurgel@forge.localhost.localdomain>
2024-03-02 19:57:19 -0300

    add(packages:queries): queries.go

--- src_go/packages/utils/queries/queries.go
+++ src_go/packages/utils/queries/queries.go
@@ -14,6 +14,7 @@ var (
 )
 
 // Check if table exists in DB
+<<<<<<< HEAD
 func TableExists(name string) bool {
 	var err error
 	conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
@@ -30,6 +31,23 @@ func TableExists(name string) bool {
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error in Query(TableExists)"+os.Getenv("$PGDATABASE")+name, err)
+=======
+func CheckExistence(name string) bool {
+	var err error
+	conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed connection to DB:Table:"+name, err)
+	}
+	rows, err = conn.Query(
+		context.Background(),
+		"SELECT * FROM information_schema.tables WHERE table_schema ='PUBLIC' and table_name = "+"'"+name+"'",
+		// "WHERE table_schema LIKE 'public'"+
+		// "AND table_type LIKE 'BASE TABLE'"+
+		// "table_name = 'credentials'",
+	)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "ERROR HERE:", err)
+>>>>>>> fd23a1e (add(packages:queries): queries.go)
 	}
 	// If not an empty array
 	for rows.Next() {
@@ -38,6 +56,7 @@ func TableExists(name string) bool {
 	// Array is empty
 	return false
 }
+<<<<<<< HEAD
 
 // Return all rows pertaining to table
 func ReturnRows(name string) pgx.Rows {
@@ -54,3 +73,5 @@ func ReturnRows(name string) pgx.Rows {
 	}
 	return rows
 }
+=======
+>>>>>>> fd23a1e (add(packages:queries): queries.go)
