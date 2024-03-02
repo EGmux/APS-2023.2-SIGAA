b36f2aba65ee5fdff64b7e20bafeb82dcb3c371e
enzogurgel <enzogurgel@forge.localhost.localdomain>
2024-03-01 21:10:57 -0300

    update(main): revised main

--- src_go/main.go
+++ src_go/main.go
@@ -1,16 +1,10 @@
 package main
 
-import (
-	"login_controller"
-)
+import "sigaa.ufpe/controllers"
 
 var db = make(map[string]string)
 
-
-
 func main() {
-	
-	login_controller.Set_Login_Controller()
+	controllers.Set_Login_Controller()
 	// Listen and Server in 0.0.0.0:8080
-	
 }
