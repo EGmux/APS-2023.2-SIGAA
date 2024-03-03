e79554964725b17b3583fa7229c5be961b6129c1
Enzo Bissoli <enzogurgel@forgeV2.localhost.localdomain>
2024-03-03 15:31:58 -0300

    fix(queries:TableExists): SQL query

--- src_go/packages/utils/queries/queries.go
+++ src_go/packages/utils/queries/queries.go
@@ -23,19 +23,21 @@ func TableExists(name string) bool {
 	var err error
 	conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error Connection("+os.Getenv("$PGDATABASE")+"):"+name)
+		fmt.Fprintln(os.Stderr, "Error Connection("+os.Getenv("PGDATABASE")+":"+name)
 	}
 	rows, err = conn.Query(
 		context.Background(),
-		"SELECT * FROM information_schema.tables"+
-			"WHERE table_schema ='PUBLIC' and table_name = "+
-			"'"+
-			name+
-			"'",
+		// WARN: Remember to space end of each string
+		"SELECT * FROM information_tables "+
+			"WHERE table_name = 'credentials'",
 	)
 	if err != nil {
+<<<<<<< HEAD
 
 		fmt.Fprintln(os.Stderr, "Error in Query(TableExists)"+os.Getenv("$PGDATABASE")+name, err)
+=======
+		fmt.Fprintln(os.Stderr, "Error in Query(TableExists)"+os.Getenv("PGDATABASE")+":"+name, err)
+>>>>>>> c14c23d (fix(queries:TableExists): SQL query)
 	}
 	// If not an empty array
 	for rows.Next() {
@@ -51,13 +53,13 @@ func ReturnRows(name string) pgx.Rows {
 	var err error
 	conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Failed Connection("+os.Getenv("$PGDATABASE")+"):"+name, err)
+		fmt.Fprintln(os.Stderr, "Failed Connection("+os.Getenv("PGDATABASE")+":"+name, err)
 	}
 	rows, err = conn.Query(context.Background(),
 		"SELECT * from "+name,
 	)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Failed Query(Return Rows):"+os.Getenv("$PGDATABASE")+":"+name, err)
+		fmt.Fprintln(os.Stderr, "Failed Query(Return Rows):"+os.Getenv("PGDATABASE")+":"+name, err)
 	}
 	return rows
 }
