6a889b9887f215ed47f8d365cd567bab009d0b34
Enzo Bissoli <enzogurgel@forgeV2.localhost.localdomain>
2024-03-08 01:02:31 -0300

    update(package:utils): abstractfactory.go, change interface name, limit return to IRepo on ReturnRepos

--- src_go/packages/utils/abstractfactory/abstractfactory.go
+++ src_go/packages/utils/abstractfactory/abstractfactory.go
@@ -1,13 +1,32 @@
 package abstractfactory
 
-type IAbstractFactory interface {
-	GetFactory()
-	CreateCourseRepo() bool
-	CreateSubjectRepo() bool
-	CreateClassRepo() bool
-	CreateEnrollmentRepo() bool
-	CreateCredentialRepo() bool
-	CreateTranscriptRepo() bool
-	CreatePROAESRepo() bool
-	CreateProfessorRepo() bool
+import (
+	"os"
+
+	"sigaa.ufpe/packages/repo"
+	"sigaa.ufpe/packages/repo/sqlrepo"
+)
+
+// Aliases
+
+type IRepoFactory interface {
+	ReturnRepos() (repo.IRepo, error)
+	CreateClassRepo() error
+	CreateEnrollmentRepo() error
+	CreateCredentialRepo() error
+	CreatePROAESRepo() error
+	CreateProfessorRepo() error
+	CreateStudentRepo() error
+}
+
+// Return Interface to
+func GetRepos() (IRepoFactory, error) {
+	tech := os.Getenv("DBTECH")
+	switch tech {
+	case "SQL":
+		return &sqlrepo.SQLRepository{}, nil
+		// case "JSON":
+		// 	return &JSONRepository{}, nil
+	}
+	return nil, nil
 }
